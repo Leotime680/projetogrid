c8e3e9d120dc4baf00908eec069603fe97f995ef
Leotime <fernandoteste680@gmail.com>
2024-07-30 00:19:10 +0100

    adicao ao codigo

--- html/testes.js
+++ html/testes.js
@@ -1,6 +1,32 @@
-var arr = [1, true, "exemplo de paragrafo", false, teste2 = {
-    nome: "pedro", sala: 4
-}]
+//criacao das variaves
+
+var nome = "leonildo"
+var dados = {
+    sala: 20,
+    classe: 2,
+    curso: "informatica",
+}
+dados.idade = 10
+console.log(dados)
+//extrututa condicional
+function controller() {
+
+    if ((nome === undefined)) {
+
+        document.write("ensira um nome valido")
+    } else if (!(nome === "leonildo")) {
+
+        document.write(" Nao e  " + nome)
+
+    } else if (nome === "leonildo" ) {
+
+        document.write(" Bem vindo  " + nome)
+        document.write(" Estes sao os teus dados " + dados)
+        console.log(" Estes sao os teus dados " + dados.nome + dados.classe)
+        document.write(" Estes sao os teus dados " + dados.nome)
 
 
-console.log(arr.length)
+    }
+}
+
+controller()
